d98d956dd9a1b526bb3dd730189794e2cb02477b
stormisprague <stormisprague@gmail.com>
2016-08-10 14:59:25 -0700

    Initial upload

---
+++ TriviaMaze/TriviaMaze/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TriviaMaze
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool correct = displayQuestion();
+
+            if (correct)
+            {
+                MessageBox.Show("Correct!");
+            }
+
+            else
+            {
+
+                button1.Enabled = false;
+                button1.Text = "Locked!";
+            }
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bool correct = displayQuestion();
+
+            
+            if (correct)
+            {
+                MessageBox.Show("Correct!");
+            }
+
+            else
+            {
+                button2.Enabled = false;
+                button2.Text = "Locked!";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            bool correct = displayQuestion();
+
+            if (correct)
+            {
+                MessageBox.Show("Correct!");
+            }
+
+            else
+            {
+                button3.Enabled = false;
+                button3.Text = "Locked!";
+            }
+        }
+
+        private bool displayQuestion()
+        {
+            bool answer;
+            using (Form2 f2 = new Form2())
+            {
+                f2.ShowDialog();
+                answer = f2.correct;
+            }
+            return answer;
+        }
+
+    }
+}
