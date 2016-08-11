f9299fc8915db51660a9fc83da1746beb7712f70
Stormi <stormisprague@gmail.com>
2016-08-10 19:57:01 -0700

    Very basic functionality. Needs lots of love.

--- TriviaMaze/TriviaMaze/Form1.Designer.cs
+++ TriviaMaze/TriviaMaze/Form1.Designer.cs
@@ -32,7 +32,16 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.trim = new System.Windows.Forms.PictureBox();
+            this.floor = new System.Windows.Forms.PictureBox();
+            this.wall = new System.Windows.Forms.PictureBox();
+            this.room_num_label = new System.Windows.Forms.Label();
+            this.d1_label = new System.Windows.Forms.Label();
+            this.d2_label = new System.Windows.Forms.Label();
+            this.d3_label = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trim)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.floor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wall)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -71,17 +80,68 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // label1
+            // trim
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Tahoma", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(117, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(442, 77);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Choose a door";
+            this.trim.Location = new System.Drawing.Point(-1, 321);
+            this.trim.Name = "trim";
+            this.trim.Size = new System.Drawing.Size(690, 17);
+            this.trim.TabIndex = 3;
+            this.trim.TabStop = false;
+            // 
+            // floor
+            // 
+            this.floor.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.floor.Location = new System.Drawing.Point(-56, 335);
+            this.floor.Name = "floor";
+            this.floor.Size = new System.Drawing.Size(761, 127);
+            this.floor.TabIndex = 4;
+            this.floor.TabStop = false;
+            // 
+            // wall
+            // 
+            this.wall.BackColor = System.Drawing.Color.LightGreen;
+            this.wall.Location = new System.Drawing.Point(-1, 0);
+            this.wall.Name = "wall";
+            this.wall.Size = new System.Drawing.Size(690, 329);
+            this.wall.TabIndex = 5;
+            this.wall.TabStop = false;
+            // 
+            // room_num_label
+            // 
+            this.room_num_label.AutoSize = true;
+            this.room_num_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.room_num_label.Location = new System.Drawing.Point(218, 43);
+            this.room_num_label.Name = "room_num_label";
+            this.room_num_label.Size = new System.Drawing.Size(223, 31);
+            this.room_num_label.TabIndex = 6;
+            this.room_num_label.Text = "ROOM NUMBER";
+            // 
+            // d1_label
+            // 
+            this.d1_label.AutoSize = true;
+            this.d1_label.Location = new System.Drawing.Point(111, 350);
+            this.d1_label.Name = "d1_label";
+            this.d1_label.Size = new System.Drawing.Size(35, 13);
+            this.d1_label.TabIndex = 7;
+            this.d1_label.Text = "label1";
+            // 
+            // d2_label
+            // 
+            this.d2_label.AutoSize = true;
+            this.d2_label.Location = new System.Drawing.Point(319, 350);
+            this.d2_label.Name = "d2_label";
+            this.d2_label.Size = new System.Drawing.Size(35, 13);
+            this.d2_label.TabIndex = 8;
+            this.d2_label.Text = "label1";
+            // 
+            // d3_label
+            // 
+            this.d3_label.AutoSize = true;
+            this.d3_label.Location = new System.Drawing.Point(539, 350);
+            this.d3_label.Name = "d3_label";
+            this.d3_label.Size = new System.Drawing.Size(35, 13);
+            this.d3_label.TabIndex = 9;
+            this.d3_label.Text = "label1";
             // 
             // Form1
             // 
@@ -90,12 +150,21 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(684, 425);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.d3_label);
+            this.Controls.Add(this.d2_label);
+            this.Controls.Add(this.d1_label);
+            this.Controls.Add(this.room_num_label);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.trim);
+            this.Controls.Add(this.floor);
+            this.Controls.Add(this.wall);
             this.Name = "Form1";
             this.Text = "Trivia Maze";
+            ((System.ComponentModel.ISupportInitialize)(this.trim)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.floor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wall)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -106,7 +175,13 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox trim;
+        private System.Windows.Forms.PictureBox floor;
+        private System.Windows.Forms.PictureBox wall;
+        private System.Windows.Forms.Label room_num_label;
+        private System.Windows.Forms.Label d1_label;
+        private System.Windows.Forms.Label d2_label;
+        private System.Windows.Forms.Label d3_label;
     }
 }
 
--- TriviaMaze/TriviaMaze/Form1.cs
+++ TriviaMaze/TriviaMaze/Form1.cs
@@ -12,12 +12,154 @@ namespace TriviaMaze
 {
     public partial class Form1 : Form
     {
+        Room[] rooms;
+        Door[] doors;
+        int startRoom = 1;
+        int endRoom = 4;
+        int curRoom;
+
         public TriviaManager manager;
+
         public Form1()
         {
             InitializeComponent();
             manager = new TriviaManager();
+
+            setup();
         }
+
+        private void setup()
+        {
+            curRoom = startRoom;
+            doors = new Door[21];
+
+            doors[0] = new Door(1, 1);
+            doors[1] = new Door(1, 5);
+            doors[2] = new Door(1, 2);
+            doors[3] = new Door(2, 6);
+            doors[4] = new Door(2, 3);
+            doors[5] = new Door(3, 7);
+            doors[6] = new Door(3, 4);
+            doors[7] = new Door(4, 8);
+            doors[8] = new Door(5, 9);
+            doors[9] = new Door(5, 6);
+            doors[10] = new Door(6, 10);
+            doors[11] = new Door(7, 8);
+            doors[12] = new Door(8, 12);
+            doors[13] = new Door(9, 13);
+            doors[14] = new Door(10, 14);
+            doors[15] = new Door(11, 15);
+            doors[16] = new Door(11, 12);
+            doors[17] = new Door(12, 16);
+            doors[18] = new Door(13, 14);
+            doors[19] = new Door(14, 15);
+            doors[20] = new Door(15, 16);
+
+
+            //empty room in room[0]. Makes 1 based indexing.
+            rooms = new Room[17];
+
+            //DOOR 0 IS A BLANK DOOR (ie a door not to display)
+            rooms[1] = new Room(1, 1, 2, 1, 0);
+            rooms[2] = new Room(2, 2, 2, 3, 4);
+            rooms[3] = new Room(3, 3, 4, 5, 6);
+            rooms[4] = new Room(4, 2, 6, 0, 7);
+            rooms[5] = new Room(5, 2, 1, 9, 8);
+            rooms[6] = new Room(6, 1, 3, 9, 10);
+            rooms[7] = new Room(7, 3, 5, 11, 0);
+            rooms[8] = new Room(8, 2, 7, 11, 12);
+            rooms[9] = new Room(9, 1, 8, 0, 13);
+            rooms[10] = new Room(10, 2, 10, 0, 14);
+            rooms[11] = new Room(11, 2, 0, 15, 16);
+            rooms[12] = new Room(12, 3, 12, 16, 17);
+            rooms[13] = new Room(13, 1, 13, 18, 0);
+            rooms[14] = new Room(14, 2, 18, 14, 19);
+            rooms[15] = new Room(15, 3, 19, 15, 20);
+            rooms[16] = new Room(2, 2, 20, 17, 0);
+
+            loadRoom(curRoom);
+
+        }
+
+        private void loadRoom(int roomNum)
+        {
+            Room cur = rooms[roomNum];
+            curRoom = roomNum;
+
+            if(roomNum == endRoom)
+            {
+                MessageBox.Show("YOU MADE IT OUT!");
+                MessageBox.Show("should do some type of replay prompt here");
+            }
+
+            room_num_label.Text = "room " + roomNum;
+
+            changeAppearance(cur.getAppearanceID());
+            setDoors(cur.door1, cur.door2, cur.door3);
+        }
+
+        private void setDoors(int d1, int d2, int d3)
+        {
+            button1.Visible = true;
+            button1.Enabled = !doors[d1].getLocked();
+            if (button1.Enabled) { button1.Text = "Open"; }
+            else { button1.Text = "Locked"; }
+            d1_label.Text = "door " + d1;
+
+            button2.Visible = true;
+            button2.Enabled = !doors[d2].getLocked();
+            if (button2.Enabled) { button2.Text = "Open"; }
+            else { button2.Text = "Locked"; }
+            d2_label.Text = "door " + d2;
+
+            button3.Visible = true;
+            button3.Enabled = !doors[d3].getLocked();
+            if (button3.Enabled) { button3.Text = "Open"; }
+            else { button3.Text = "Locked"; }
+            d3_label.Text = "door " + d3;
+
+            if(d1 == 0) { button1.Visible = false; button1.Enabled = false; }
+            if (d2 == 0) { button2.Visible = false; button2.Enabled = false; }
+            if (d3 == 0) { button3.Visible = false; button3.Enabled = false; }
+        }
+
+        private void changeAppearance(int aNum)
+        {
+            if (aNum == 1)
+            {
+                wall.BackColor = Color.LightSeaGreen;
+                trim.BackColor = Color.White;
+                floor.BackColor = Color.Gray;
+            }
+            else if(aNum == 2)
+            {
+                wall.BackColor = Color.DeepSkyBlue;
+                trim.BackColor = Color.White;
+                floor.BackColor = Color.Beige;
+            }
+            else
+            {
+                wall.BackColor = Color.HotPink;
+                trim.BackColor = Color.White;
+                floor.BackColor = Color.Gray;
+            }
+        }
+
+        private void gameOver()
+        {
+            MessageBox.Show("You died in the woods. Sorry not sorry");
+        }
+
+        private bool allDoorsLocked()
+        {
+           if(button1.Enabled || button2.Enabled || button3.Enabled)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -26,6 +168,11 @@ namespace TriviaMaze
             if (correct)
             {
                 MessageBox.Show("Correct!");
+                int curDoor = rooms[curRoom].door1;
+                int nextRoom = doors[curDoor].getNextRoom(curRoom);
+
+                loadRoom(nextRoom);
+                
             }
 
             else
@@ -33,6 +180,8 @@ namespace TriviaMaze
 
                 button1.Enabled = false;
                 button1.Text = "Locked!";
+                doors[rooms[curRoom].door1].setLocked(true);
+                if (allDoorsLocked()) { gameOver(); }
             }
            
         }
@@ -45,12 +194,19 @@ namespace TriviaMaze
             if (correct)
             {
                 MessageBox.Show("Correct!");
+                int curDoor = rooms[curRoom].door2;
+                int nextRoom = doors[curDoor].getNextRoom(curRoom);
+
+                loadRoom(nextRoom);
             }
 
             else
             {
                 button2.Enabled = false;
                 button2.Text = "Locked!";
+
+                doors[rooms[curRoom].door2].setLocked(true);
+                if (allDoorsLocked()) { gameOver(); }
             }
         }
 
@@ -61,12 +217,18 @@ namespace TriviaMaze
             if (correct)
             {
                 MessageBox.Show("Correct!");
+                int curDoor = rooms[curRoom].door3;
+                int nextRoom = doors[curDoor].getNextRoom(curRoom);
+
+                loadRoom(nextRoom);
             }
 
             else
             {
                 button3.Enabled = false;
                 button3.Text = "Locked!";
+                doors[rooms[curRoom].door3].setLocked(true);
+                if (allDoorsLocked()) { gameOver(); }
             }
         }
 
